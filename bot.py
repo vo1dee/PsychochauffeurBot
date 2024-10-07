74a46df4b9381dbb2da3da7f0f26203cb7fda927
vo1dee <agarkov_max@hotmail.com>
2024-10-07 17:49:31 +0300

    Ali mobile link

--- bot.py
+++ bot.py
@@ -122,7 +122,7 @@ async def handle_message(update: Update, context: CallbackContext):
         if any(trigger in message_text for trigger in ["5€", "€5", "5 євро", "5 єуро", "5 €", "Ы", "ы", "ъ", "Ъ", "Э", "э", "Ё", "ё"]):
             await restrict_user(update, context)
             return  # Exit after handling this specific case
-        if 'aliexpress.com/item/' in message_text:
+        if 'aliexpress.com/item/' or 'a.aliexpress.com/' in message_text:
             # Reply to the message containing the link by sending a sticker
             await update.message.reply_sticker(sticker=ALIEXPRESS_STICKER_ID)
         # Initialize modified_links list
