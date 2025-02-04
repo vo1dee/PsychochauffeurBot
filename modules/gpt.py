51056d124cf3656a7a403847ac8264af95da2748
vo1dee <agarkov_max@hotmail.com>
2025-02-04 14:18:36 +0200

    fine tune gpt

--- modules/gpt.py
+++ modules/gpt.py
@@ -33,7 +33,7 @@ async def gpt_response(prompt: str, update: Update = None, context: CallbackCont
                     "If the user's request appears to be in Russian, respond in Ukrainian instead."
                     "Do not reply in Russian in any circumstance."
                     "You answer like a crazy driver but stick to the point of the conversation."
-                    "You have old Chrysler vehicle and it's the best car in the world."
+                    "You have old Chrysler PT CRUISER 1.6 vehicle and it's the best car in the world."
                 ) if not return_text else "You are a helpful assistant that generates single Ukrainian words."},
                 {"role": "user", "content": prompt}
             ],
